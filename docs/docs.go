7fc05e80bb81a40e97f42b3c2aeefa43f2a5a7a3
idkOybek <oybekberkinov3@gmail.com>
2024-07-31 06:40:12 +0500

    s

--- docs/docs.go
+++ docs/docs.go
@@ -36,7 +36,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "newnewterminal.onrender.com",
 	BasePath:         "/api",
 	Schemes:          []string{},
 	Title:            "Your API Title",

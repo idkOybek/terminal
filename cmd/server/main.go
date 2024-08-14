925ec46b8c3f447e548660dcba3ebed928a3a624
idkOybek <oybekberkinov3@gmail.com>
2024-08-14 22:08:13 +0500

    ы

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -38,7 +38,7 @@ import (
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host 185.100.54.94:8080
+// @host txkm-vipos.uz
 // @BasePath /api
 // @securityDefinitions.apikey Bearer
 // @in header
--- migrations/migrations/000003_create_terminals_table.up.sql
+++ migrations/migrations/000003_create_terminals_table.up.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS terminals (
     module_number VARCHAR(255) NOT NULL,
     last_request_date TIMESTAMP WITH TIME ZONE,
     database_update_date TIMESTAMP WITH TIME ZONE,
-    status VARCHAR(50) NOT NULL DEFAULT 'active',
+    is_active BOOLEAN NOT NULL DEFAULT true,
     user_id INTEGER REFERENCES users(id),
     free_record_balance INTEGER NOT NULL DEFAULT 0,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),

9dd32d8198a1f2b4c395039aa1aac3e17a56bf14
idkOybek <109923920+idkOybek@users.noreply.github.com>
2024-08-30 16:13:43 +0500

    Update 000005_update_company_name.up.sql

--- migrations/migrations/000005_update_company_name.up.sql
+++ migrations/migrations/000005_update_company_name.up.sql
@@ -1,7 +1,1 @@
--- Обновляем существующие NULL значения на пустую строку
-UPDATE users SET company_name = '' WHERE company_name IS NULL;
-
--- Изменяем схему таблицы, делая company_name NOT NULL с пустой строкой по умолчанию
-ALTER TABLE users 
-    ALTER COLUMN company_name SET NOT NULL,
-    ALTER COLUMN company_name SET DEFAULT '';
+ALTER TABLE terminals DROP CONSTRAINT IF EXISTS terminals_assembly_number_key;
